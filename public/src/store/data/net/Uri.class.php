9febece2f8f6ea121cc7a28762e2f486451f5503
Michael Piontkowski <michael.piontkowski@1und1.de>
2016-01-13 23:38:45 +0100

    URI-Data class

---
+++ public/src/store/data/net/Uri.class.php
@@ -0,0 +1,23 @@
+<?php
+namespace store\data\net;
+
+/**
+ * @TODO write unit-tests
+ */
+class Uri extends store\data\AbstractData {
+	
+	const PATTERN = '[A-Za-z0-9-._~:\/?#\[\]@!$&\'()*+,;=]+';
+	
+	/**
+	 * @param string $uri
+	 * @return Uri
+	 */
+	protected function set($uri) {
+		if (!is_string($uri) || !self::match(self::PATTERN, $uri)) {
+			throw new RuntimeException('URI `'.$uri.'` is not valid');
+		}
+		$this->data = $uri;
+		return $this;
+	}
+	
+}
