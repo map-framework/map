5693c5aa3aa99f08c33efc4c421a1d385cdd93bc
mpiontkowski <mail@mpiontkowski.de>
2016-02-23 15:18:13 +0100

    remove MySQL types

--- public/src/peer/mysql/AbstractTable.class.php
+++ public/src/peer/mysql/AbstractTable.class.php
@@ -8,68 +8,14 @@ use store\Bucket;
 
 abstract class AbstractTable {
 
-	# Integer
-	const TYPE_TINYINT   = 'TINYINT';
-	const TYPE_SMALLINT  = 'SMALLINT';
-	const TYPE_MEDIUMINT = 'MEDIUMINT';
-	const TYPE_INT       = 'INT';
-	const TYPE_BIGINT    = 'BIGINT';
-
-	# Float-Number
-	const TYPE_FLOAT   = 'FLOAT';
-	const TYPE_DOUBLE  = 'DOUBLE';
-	const TYPE_DECIMAL = 'DECIMAL';
-
-	# String
-	const TYPE_CHAR    = 'CHAR';
-	const TYPE_VARCHAR = 'VARCHAR';
-	const TYPE_TEXT    = 'TEXT';
-
-	# Date & Time
-	const TYPE_TIMESTAMP = 'TIMESTAMP';
-	const TYPE_DATETIME  = 'DATETIME';
-	const TYPE_DATE      = 'DATE';
-	const TYPE_TIME      = 'TIME';
-	const TYPE_YEAR      = 'YEAR';
-
-	# Lists
-	const LIST_INT    = array(
-			self::TYPE_TINYINT,
-			self::TYPE_SMALLINT,
-			self::TYPE_MEDIUMINT,
-			self::TYPE_INT,
-			self::TYPE_BIGINT
-	);
-	const LIST_FLOAT  = array(
-			self::TYPE_FLOAT,
-			self::TYPE_DOUBLE,
-			self::TYPE_DECIMAL
-	);
-	const LIST_STRING = array(
-			self::TYPE_CHAR,
-			self::TYPE_VARCHAR,
-			self::TYPE_TEXT
-	);
-	const LIST_DATE   = array(
-			self::TYPE_TIMESTAMP,
-			self::TYPE_DATETIME,
-			self::TYPE_DATE,
-			self::TYPE_TIME,
-			self::TYPE_YEAR
-	);
-
-	# Format Patterns
-	const FORMAT_DATETIME = 'Y-m-d h:i:s';
-	const FORMAT_DATE     = 'Y-m-d';
-	const FORMAT_TIME     = 'h:i:s';
-	const FORMAT_YEAR     = 'Y';
-
 	/**
 	 * @var Bucket
 	 */
 	protected $config = null;
 
 	/**
+	 * custom table name
+	 *
 	 * @var string
 	 */
 	private $tableName = null;
@@ -178,72 +124,33 @@ abstract class AbstractTable {
 	 * @return AbstractTable this
 	 */
 	final public function clear() {
-		$this->content = new Bucket();
-		$this->pointer = 0;
-		return $this;
-	}
-
-	/**
-	 * @param  string $columnName
-	 * @param  mixed  $content
-	 * @param  int    $pointer default self::getPointer()
-	 * @throws RuntimeException
-	 * @return AbstractTable this
-	 */
-	final public function set($columnName, $content, $pointer = null) {
-		if (!isset($this->columnList[$columnName])) {
-			throw new RuntimeException('column `'.$columnName.'` not exists');
-		}
-		$type = $this->columnList[$columnName]['type'];
-
-		if (in_array($type, self::LIST_INT)) {
-			# Integer
-			$content = (int) $content;
-		}
-		elseif (in_array($type, self::LIST_FLOAT)) {
-			# Float-Number
-			$content = (float) $content;
-		}
-		elseif (in_array($type, self::LIST_STRING)) {
-			# String
-			$content = (string) $content;
-		}
-		elseif (in_array($type, self::LIST_DATE)) {
-			# Date & Time
-			if (!($content instanceof DateTime)) {
-				throw new RuntimeException('content is not instance of `\DateTime`');
-			}
-
-			if ($type === self::TYPE_DATETIME) {
-				$content = $content->format(self::FORMAT_DATETIME);
-			}
-			elseif ($type === self::TYPE_DATE) {
-				$content = $content->format(self::FORMAT_DATE);
-			}
-			elseif ($type === self::TYPE_TIME) {
-				$content = $content->format(self::FORMAT_TIME);
-			}
-			elseif ($type === self::TYPE_YEAR) {
-				$content = $content->format(self::FORMAT_YEAR);
-			}
-			else {
-				$content = $content->getTimestamp();
-			}
-		}
-		else {
-			throw new RuntimeException('type `'.$type.'` not valid');
-		}
-
-		if ($pointer === null) {
-			$pointer = $this->getPointer();
-		}
-		$this->content->set($pointer, $columnName, $content);
+		$this->content  = new Bucket();
+		$this->pointer  = 0;
+		$this->isFilled = false;
 		return $this;
 	}
 
 	/**
 	 * @param  string   $columnName
-	 * @param  int|null $pointer
+	 * @param  mixed    $value
+	 * @param  int|null $pointer default this::getPointer()
+	 * @throws RuntimeException
+	 * @return AbstractTable this
+	 */
+	final public function set($columnName, $value, $pointer = null) {
+		if (!isset($this->columnList[$columnName])) {
+			throw new RuntimeException('column `'.$columnName.'` not exists');
+		}
+		if ($pointer === null) {
+			$pointer = $this->getPointer();
+		}
+		$this->content->set($pointer, $columnName, $value);
+		return $this;
+	}
+
+	/**
+	 * @param  string   $columnName
+	 * @param  int|null $pointer default this::getPointer()
 	 * @return mixed
 	 */
 	final public function get($columnName, $pointer = null) {
