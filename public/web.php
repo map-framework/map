dc47ed622974794ad7fd776598f72a44ec9d979a
mpiontkowski <mail@mpiontkowski.de>
2016-03-14 19:57:42 +0100

    - fill constant ENVIRONMENT with 'DEV' or 'LIVE' (#24) - rewrite loading process of configs:
    	1. public config
    	2. private config
    	3. area config
    	4. page config
    	5. session config

--- public/web.php
+++ public/web.php
@@ -18,22 +18,59 @@ final class Web {
 	private $config;
 
 	/**
+	 * @var MAPUrl
+	 */
+	private $request;
+
+	/**
 	 * initialize application and load configs
 	 */
 	public function __construct() {
 		include_once self::AUTOLOAD;
+
+		if (strtolower(ini_get('display_errors')) === 'off') {
+			define('ENVIRONMENT', 'LIVE');
+		}
+		else {
+			define('ENVIRONMENT', 'DEV');
+		}
 
 		if (!session_start()) {
 			Logger::error('failed to start session');
 			exit();
 		}
 
-		$this->config = new Bucket();
-		# apply public config
-		$this->config->applyIni(new File(self::CONFIG_PUBLIC));
-		# apply private config
-		$this->config->applyIni(new File(self::CONFIG_PRIVATE));
-		# apply session config
+		# load public & private config
+		$this->config = (new Bucket())
+				->applyIni(new File(self::CONFIG_PUBLIC))
+				->applyIni(new File(self::CONFIG_PRIVATE));
+
+		# load MAPUrl
+		if (stripos($_SERVER['SERVER_PROTOCOL'], 'HTTPS') !== false) {
+			$scheme = 'https';
+		}
+		else {
+			$scheme = 'http';
+		}
+		$this->request = new MAPUrl($scheme.'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'], $this->config);
+
+		# load area & page config
+		$configPathList = array(
+				'../private/src/area/'.$this->request->getArea().'/config/area.ini',
+				'../private/src/area/'.$this->request->getArea().'/config/page/'.$this->request->getPage().'.ini'
+		);
+		foreach ($configPathList as $configPath) {
+			$configFile = new File($configPath);
+			if (!$configFile->isFile()) {
+				if (constant('ENVIRONMENT') === 'DEV') {
+					Logger::info('config-file `'.$configFile.'` not found');
+				}
+				continue;
+			}
+			$this->config->applyIni($configFile);
+		}
+
+		# load session config
 		if (!isset($_SESSION['config'])) {
 			$_SESSION['config'] = array();
 		}
@@ -47,29 +84,21 @@ final class Web {
 	 * @return void
 	 */
 	public function main() {
-		if (stripos($_SERVER['SERVER_PROTOCOL'], 'HTTPS') !== false) {
-			$scheme = 'https';
-		}
-		else {
-			$scheme = 'http';
-		}
-
-		$request      = new MAPUrl($scheme.'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'], $this->config);
-		$modeSettings = $this->config->get('mode', $request->getMode());
+		$modeSettings = $this->config->get('mode', $this->request->getMode());
 
 		if ($modeSettings === null) {
-			throw new RuntimeException('mode `'.$request->getMode().'` not applied');
+			throw new RuntimeException('mode `'.$this->request->getMode().'` not applied');
 		}
 
 		if (!class_exists($modeSettings['handler'])) {
-			throw new RuntimeException('mode `'.$request->getMode().'` handler `'.$modeSettings['handler'].'` not applied');
+			throw new RuntimeException('mode `'.$this->request->getMode().'` is invalid');
 		}
 
-		$handler = new $modeSettings['handler']($this->config, $request, $modeSettings);
+		$handler = new $modeSettings['handler']($this->config, $this->request, $modeSettings);
 		if (!($handler instanceof AbstractModeHandler)) {
 			throw new RuntimeException(
 					'mode `'
-					.$request->getMode()
+					.$this->request->getMode()
 					.'` handler `'
 					.$modeSettings['handler']
 					.'` is not instance of `handler\mode\AbstractModeHandler`'
