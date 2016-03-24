da90d430f966fb08ed33330307d7f951e2a1ad04
mpiontkowski <mail@mpiontkowski.de>
2016-03-24 15:52:14 +0100

    create invalid value exception (#28)

---
+++ public/src/exception/InvalidValueException.class.php
@@ -0,0 +1,19 @@
+<?php
+namespace exception;
+
+use Throwable;
+
+/**
+ * This file is part of the MAP-Framework.
+ *
+ * @author    Michael Piontkowski <mail@mpiontkowski.de>
+ * @copyright Copyright 2016 Michael Piontkowski
+ * @license   https://raw.githubusercontent.com/map-framework/map/master/LICENSE.txt Apache License 2.0
+ */
+class InvalidValueException extends MAPException implements Throwable {
+
+	public function __construct(string $expected, $value) {
+		parent::__construct('invalid value '.$this->export($value).' (expected: '.$this->export($expected).')');
+	}
+
+}
