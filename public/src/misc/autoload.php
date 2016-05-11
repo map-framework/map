bb781afa95249317217edbc0f8d219079cb87f51
mpiontkowski <mail@mpiontkowski.de>
2016-05-11 17:06:58 +0200

    adjust Autoloader (#33)

--- public/src/misc/autoload.php
+++ public/src/misc/autoload.php
@@ -7,22 +7,72 @@
  * @copyright Copyright 2016 Michael Piontkowski
  * @license   https://raw.githubusercontent.com/map-framework/map/master/LICENSE.txt Apache License 2.0
  */
-function mapAutoload(string $namespace):bool {
-	$fileSuffix    = '.class.php';
-	$directoryList = array(
-			'public/src/',
-			'private/src/'
-	);
-	$itemList      = explode('\\', $namespace);
+final class MAPAutoloader {
 
-	foreach ($directoryList as $directory) {
-		$path = '../'.$directory.implode('/', $itemList).$fileSuffix;
-		if (file_exists($path)) {
-			include_once $path;
+	const PATH_ROOT   = '../';
+	const PATH_SOURCE = 'src/';
+
+	const PATH_PUBLIC  = 'public/';
+	const PATH_ADDONS  = 'public/addon/';
+	const PATH_PRIVATE = 'private/';
+
+	const FILE_EXTENSION = '.class.php';
+
+	/**
+	 * @var array
+	 */
+	private static $pathList = array();
+
+	public static function addPath(string $path):bool {
+		if (is_dir(self::PATH_ROOT.$path)) {
+			self::$pathList[] = self::PATH_ROOT.$path;
 			return true;
 		}
+		return false;
 	}
-	return false;
+
+	public static function clearPathList() {
+		self::$pathList = array();
+	}
+
+	public static function refreshPathList() {
+		self::clearPathList();
+
+		# Public
+		self::addPath(self::PATH_PUBLIC.self::PATH_SOURCE);
+
+		# Add-Ons
+		if (is_dir(self::PATH_ROOT.self::PATH_ADDONS)) {
+			$addOnNameList = scandir(self::PATH_ROOT.self::PATH_ADDONS);
+			foreach ($addOnNameList as $addOnName) {
+				if ($addOnName === '.' || $addOnName === '..') {
+					continue;
+				}
+				if (!is_dir(self::PATH_ROOT.self::PATH_ADDONS.$addOnName)) {
+					continue;
+				}
+				self::addPath(self::PATH_ADDONS.$addOnName.'/'.self::PATH_SOURCE);
+			}
+		}
+
+		# Private
+		self::addPath(self::PATH_PRIVATE.self::PATH_SOURCE);
+	}
+
+	public static function load(string $nameSpace):bool {
+		$filePath = str_replace('\\', '/', $nameSpace).self::FILE_EXTENSION;
+
+		foreach (array_reverse(self::$pathList) as $path) {
+			if (is_file($path.$filePath)) {
+				/** @noinspection PhpIncludeInspection */
+				include_once $path.$filePath;
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
 
-spl_autoload_register('mapAutoload', true);
+MAPAutoloader::refreshPathList();
+spl_autoload_register('MAPAutoloader::load', true);
