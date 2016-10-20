6e0cb8025a50f45262aaff79311399c4319c3d21
Michael Piontkowski <mail@mpiontkowski.de>
2016-10-20 15:04:53 +0200

    Update ClassObject.class.php
    fix deprecated method call

--- public/src/data/oop/ClassObject.class.php
+++ public/src/data/oop/ClassObject.class.php
@@ -63,7 +63,7 @@ class ClassObject extends AbstractData {
 	 */
 	final public function getAnnotation(string $name):Annotation {
 		foreach ($this->getAnnotationList() as $annotation) {
-			if ($annotation->getName() === $name) {
+			if ($annotation->get() === $name) {
 				return $annotation;
 			}
 		}
